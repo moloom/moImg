2b3fe0777e83891ec6cb256008e336a9c6cbf293
moloom <moloom777@gmail.com>
2024-09-28 21:53:52 +0800

    test the project connection

---
+++ api/src/main/java/com/moloom/img/api/controller/Test.java
@@ -0,0 +1,21 @@
+package com.moloom.img.api.controller;
+
+import com.alibaba.fastjson.JSONArray;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: moloom
+ * @date: 2024-09-23 21:20
+ * @description:
+ */
+@RestController
+@RequestMapping("/i")
+public class Test {
+    @RequestMapping(value = "/test",method = RequestMethod.GET)
+    public String TestConnection(){
+
+        return JSONArray.toJSONString("Welcome MOSS!");
+    }
+}
