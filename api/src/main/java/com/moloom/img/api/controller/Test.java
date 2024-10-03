1aa3f89acd5eb62f0e477db2adbc46089d594678
moloom <moloom777@gmail.com>
2024-10-03 19:31:43 +0800

    test redis connection

--- api/src/main/java/com/moloom/img/api/controller/Test.java
+++ api/src/main/java/com/moloom/img/api/controller/Test.java
@@ -1,9 +1,15 @@
 package com.moloom.img.api.controller;
 
 import com.alibaba.fastjson.JSONArray;
+import com.moloom.img.api.config.RedisConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @author: moloom
@@ -13,9 +19,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/i")
 public class Test {
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     public String TestConnection() {
+        // 获取当前时间
+        LocalDateTime currentTime = LocalDateTime.now();
 
+        // 定义日期时间格式
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        // 格式化当前时间为字符串
+        String formattedTime = currentTime.format(formatter);
+
+        redisTemplate.opsForValue().set("currentTime", formattedTime);
         return JSONArray.toJSONString("Welcome to moImg!");
     }
 }
