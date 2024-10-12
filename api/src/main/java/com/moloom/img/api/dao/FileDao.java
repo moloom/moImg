b4b6523b26788524552450936f72c41b90edb876
moloom <moloom777@gmail.com>
2024-10-12 23:56:50 +0800

    init

---
+++ api/src/main/java/com/moloom/img/api/dao/FileDao.java
@@ -0,0 +1,9 @@
+package com.moloom.img.api.dao;
+
+/**
+ * @author: moloom
+ * @date: 2024-10-12 23:56
+ * @description:
+ */
+public interface FileDao {
+}
---
+++ api/src/main/java/com/moloom/img/api/service/File.java
@@ -0,0 +1,9 @@
+package com.moloom.img.api.service;
+
+/**
+ * @author: moloom
+ * @date: 2024-10-12 23:54
+ * @description:
+ */
+public interface File {
+}
