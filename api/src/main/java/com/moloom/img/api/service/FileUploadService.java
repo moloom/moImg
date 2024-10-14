dbceb233813888fb58ce1fbcd0d9482333cca71a
moloom <moloom777@gmail.com>
2024-10-14 19:46:01 +0800

    init

---
+++ api/src/main/java/com/moloom/img/api/service/FileUploadService.java
@@ -0,0 +1,9 @@
+package com.moloom.img.api.service;
+
+/**
+ * @author: moloom
+ * @date: 2024-10-14 19:43
+ * @description:
+ */
+public interface FileUploadService extends FileService {
+}
---
+++ api/src/main/java/com/moloom/img/api/service/impl/FileDownloadService.java
@@ -0,0 +1,11 @@
+package com.moloom.img.api.service.impl;
+
+import com.moloom.img.api.service.FileService;
+
+/**
+ * @author: moloom
+ * @date: 2024-10-14 19:45
+ * @description:
+ */
+public interface FileDownloadService extends FileService {
+}
