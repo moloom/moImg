b1caadaaaaad2311a423e27c4c4d4f83c376283a
moloom <moloom777@gmail.com>
2024-10-15 05:18:06 +0800

    rename to StringGenerator

--- api/src/main/java/com/moloom/img/api/utils/StringGenerator.java
+++ api/src/main/java/com/moloom/img/api/utils/StringGenerator.java
@@ -3,16 +3,17 @@ package com.moloom.img.api.utils;
 /**
  * @author: moloom
  * @date: 2024-10-13 23:49
- * @description: token 生成器
+ * @description: 随即字符串 生成器
  */
-public class TokenGenerator {
+public class StringGenerator {
 
     // 基础字符
     private static final String CHARACTERS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    // token的长度
-    private static final int TOKEN_LENGTH = 32;
     // 字符集的长度
     private static final int CHARACTERS_LENGTH = CHARACTERS.length();
+    // token的长度
+    private static final int TOKEN_LENGTH = 32;
+
 
     public static String getToken() {
         //StringBuilder 比 StringBuffer 几乎快一倍
