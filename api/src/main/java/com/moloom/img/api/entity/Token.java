aa4bbd0e13185ab9b476b2a265b1a21870da8f47
moloom <moloom777@gmail.com>
2024-10-19 23:42:36 +0800

    init token entity

---
+++ api/src/main/java/com/moloom/img/api/entity/Token.java
@@ -0,0 +1,13 @@
+package com.moloom.img.api.entity;
+
+/**
+ * @author: moloom
+ * @date: 2024-10-19 23:41
+ * @description:
+ */
+public class Token {
+    private String token;
+    private String userId;
+    private String status;
+    private String createdTime;
+}
