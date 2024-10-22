982c6c02375f6ae48905dc1ac896822ae54bedee
moloom <moloom777@gmail.com>
2024-10-23 00:49:03 +0800

    upload img to minio

--- api/src/main/java/com/moloom/img/api/service/impl/ImgHandlerServiceImpl.java
+++ api/src/main/java/com/moloom/img/api/service/impl/ImgHandlerServiceImpl.java
@@ -4,15 +4,22 @@ import com.moloom.img.api.entity.ImgInfo;
 import com.moloom.img.api.service.ImgHandlerService;
 import com.moloom.img.api.to.R;
 import com.moloom.img.api.vo.FileUploadVo;
-import io.minio.MinioClient;
-import io.minio.ObjectWriteResponse;
-import io.minio.PutObjectArgs;
+import io.minio.*;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tika.Tika;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.metadata.TikaMimeKeys;
+import org.apache.tika.parser.Parser;
+import org.apache.tika.sax.BodyContentHandler;
+import org.xml.sax.helpers.DefaultHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * @author: moloom
@@ -34,13 +41,38 @@ public class ImgHandlerServiceImpl implements ImgHandlerService {
     private String prePath;
 
     @Override
-    public R imghandler(FileUploadVo fileUploadVo) {
+    public R imghandler(FileUploadVo fileUploadVo) throws Exception {
         /*
          * 1.
          *
          */
+        // 提取元信息
+        /*try (InputStream inputStream = fileUploadVo.getMultipartFile().getInputStream()) {
+            Metadata metadata = new Metadata();
+            BodyContentHandler handler = new BodyContentHandler();
+            // 使用 Tika 处理输入流
+            Tika tika = new Tika();
+            tika.parse(inputStream, metadata);
 
-        return null;
+            // 获取元信息
+            String cameraModel = metadata.get("Exif.Image.Model");
+            String dateTimeOriginal = metadata.get("Exif.Image.DateTimeOriginal");
+            // 处理和存储元信息
+        } catch (Exception e) {
+            // 处理异常
+        }*/
+        boolean bucketExists = minioClient.bucketExists(BucketExistsArgs.builder().bucket("images").build());
+        if (!bucketExists) {
+            minioClient.makeBucket(MakeBucketArgs.builder().bucket("images").build());
+        } else
+            System.out.println("bucket qwertyuiop is exists");
+        ObjectWriteResponse flag = minioClient.putObject(PutObjectArgs.builder()
+                .bucket("images")
+                .stream(fileUploadVo.getMultipartFile().getInputStream(), fileUploadVo.getMultipartFile().getSize(), -1).object(fileUploadVo.getMultipartFile().getName())
+                .build());
+        System.out.println(flag.etag() + "\n" + flag.bucket() + "\n" + flag.versionId());
+
+        return R.success(flag);
     }
 
     @Override
