8d9e48b320f827d65f08fda37a7085ac0b4a1dd2
moloom <moloom777@gmail.com>
2024-10-22 15:28:45 +0800

    add @Data

--- api/src/main/java/com/moloom/img/api/entity/Token.java
+++ api/src/main/java/com/moloom/img/api/entity/Token.java
@@ -1,13 +1,18 @@
 package com.moloom.img.api.entity;
 
+import lombok.Data;
+
+import java.sql.Timestamp;
+
 /**
  * @author: moloom
  * @date: 2024-10-19 23:41
  * @description:
  */
+@Data
 public class Token {
     private String token;
-    private String userId;
-    private String status;
-    private String createdTime;
+    private Long userId;
+    private Byte status;
+    private Timestamp createdTime;
 }
