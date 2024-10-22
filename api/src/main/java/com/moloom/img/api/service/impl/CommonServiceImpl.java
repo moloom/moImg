8129f9d4f2fda6fa735faf41f80487be09bf4059
moloom <moloom777@gmail.com>
2024-10-23 00:51:43 +0800

    add service annotation

--- api/src/main/java/com/moloom/img/api/service/impl/CommonServiceImpl.java
+++ api/src/main/java/com/moloom/img/api/service/impl/CommonServiceImpl.java
@@ -3,19 +3,21 @@ package com.moloom.img.api.service.impl;
 import com.moloom.img.api.dao.CommonDao;
 import com.moloom.img.api.service.CommonService;
 import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
 
 /**
  * @author: moloom
  * @date: 2024-10-19 22:40
  * @description:
  */
+@Service
 public class CommonServiceImpl implements CommonService {
 
     @Resource
-    private CommonDao dao;
+    private CommonDao commonDao;
 
     @Override
     public boolean isTokenExist(String token) {
-        return false;
+        return commonDao.isTokenExist(token);
     }
 }
