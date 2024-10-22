a7314f8ff6c6863b3b63ee759425a769df6c4224
moloom <moloom777@gmail.com>
2024-10-22 18:15:01 +0800

    init

---
+++ api/src/main/java/com/moloom/img/api/vo/FileUploadVo.java
@@ -0,0 +1,18 @@
+package com.moloom.img.api.vo;
+
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @author: moloom
+ * @date: 2024-10-22 17:41
+ * @description:
+ */
+@Data
+public class FileUploadVo {
+    private String fileName;    //如果是在页面上传文件，页面会获取文件名，若是api则需自己获取
+    private String fileExtension; //如果是在页面上传文件，页面会获取后缀，若是api则需自己获取
+    private MultipartFile multipartFile;
+    private String token;
+    private ImgActionsVo imgActionsVo;
+}
---
+++ api/src/main/java/com/moloom/img/api/vo/ImgActionsVo.java
@@ -0,0 +1,15 @@
+package com.moloom.img.api.vo;
+
+import lombok.Data;
+
+/**
+ * @author: moloom
+ * @date: 2024-10-22 18:06
+ * @description:
+ */
+@Data
+public class ImgActionsVo {
+    private Integer width;
+    private Integer height; //
+    private Float scale;    //等比缩放比例
+}
