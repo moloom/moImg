7c10caf7ca4d9c4be4b8f17c1bfa0b637c9cd727
moloom <moloom777@gmail.com>
2024-10-23 00:52:05 +0800

    update args

--- api/src/main/java/com/moloom/img/api/controller/UploadController.java
+++ api/src/main/java/com/moloom/img/api/controller/UploadController.java
@@ -1,15 +1,17 @@
 package com.moloom.img.api.controller;
 
+import com.moloom.img.api.exception.ExtensionMismatchException;
+import com.moloom.img.api.service.UploadDispatcherService;
+import com.moloom.img.api.to.R;
+import com.moloom.img.api.service.CommonService;
+import com.moloom.img.api.vo.FileUploadVo;
 import io.minio.MinioClient;
-import io.minio.ObjectWriteResponse;
-import io.minio.PutObjectArgs;
-import io.minio.UploadObjectArgs;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -20,38 +22,37 @@ import org.springframework.web.multipart.MultipartFile;
  * @description: receive upload file
  */
 @RestController
-@RequestMapping(method = RequestMethod.GET)
+@Slf4j
 public class UploadController {
 
     @Resource
     private RedisTemplate<String, Object> redisTemplate;
 
-    @Resource
-    private MinioClient minioClient;
+    @Autowired
+    private CommonService commonService;
 
-    @Value("${moimg.upload.upload-root-path}")
-    private String path;
+    @Autowired
+    private UploadDispatcherService imgUploadDispatcherService;
 
     @RequestMapping(value = "/upload/{token}", method = RequestMethod.POST, consumes = "multipart/form-data")
-    public String upload(@RequestParam("file") MultipartFile multipartFile,
-                         HttpServletRequest request,
-                         HttpSession session,
-                         @PathVariable String token) throws Exception {
+    public R upload(FileUploadVo fileUploadVo, @RequestParam("file") MultipartFile multipartFile,
+                    @PathVariable("token") String token,
+                    HttpServletRequest request) throws Exception {
         //打印所有的参数
-        System.out.println("上传地址:" + path);
-        System.out.println("request.getRemoteHost() = " + request.getRemoteHost());
-        System.out.println("X-Forwarded-For:" + request.getHeader("X-Forwarded-For"));
-        System.out.println("X-real-ip:" + request.getHeader("X-Real-IP"));
-
-        ObjectWriteResponse flag = minioClient.putObject(PutObjectArgs.builder()
-                .bucket("img")
-                .stream(multipartFile.getInputStream(), multipartFile.getSize(), -1).object("/1/2/3/4.jpg")
-                .build());
-        System.out.println(flag.etag() + "\n" + flag.bucket() + "\n" + flag.versionId());
-
-//        System.out.println("multipartFile 是否为空：" + multipartFile == null);
-//        multipartFile.transferTo(new File(path + "/f2.jpeg"));
-        return "hahaha~";
+        log.info("request.getRemoteHost() = " + request.getRemoteHost());
+        log.info("X-Forwarded-For:" + request.getHeader("X-Forwarded-For"));
+        log.info("X-real-ip:" + request.getHeader("X-Real-IP"));
+        fileUploadVo.setToken(token);
+        fileUploadVo.setMultipartFile(multipartFile);
+        log.debug(fileUploadVo.toString());
+        R r = null;
+        try {
+            r = imgUploadDispatcherService.uploadDispatcher(fileUploadVo);
+        } catch (ExtensionMismatchException e) {
+            e.printStackTrace();
+            return R.error(e.getMessage());
+        }
+        return r;
     }
 
 /*    @RequestMapping(value = "/uploadd/{token}", method = RequestMethod.POST, consumes = "multipart/form-data")
