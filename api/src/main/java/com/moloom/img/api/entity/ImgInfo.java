adf1b4118095c1e2b7f541747234473530e8850a
moloom <moloom777@gmail.com>
2024-10-31 22:29:51 +0800

    add some members

--- api/src/main/java/com/moloom/img/api/entity/ImgInfo.java
+++ api/src/main/java/com/moloom/img/api/entity/ImgInfo.java
@@ -12,16 +12,20 @@ import java.sql.Timestamp;
 @Data
 public class ImgInfo {
     private Long imgId;
-    private String fileName;
-    private String fileUrl;
-    private Long fileSize;
-    private String fileExtension;
-    private Long imgWidth;
-    private Long imgHeight;
-    private String camera_info;
+    private String originalFullName;
+    private String storageFullName;
+    private String storagePath;
+    private Long size;
+    private String extension;
+    private String token;
+    private Long width;
+    private Long height;
+    private String cameraInfo;
     private Float latitude;
     private Float longitude;
     private Float altitude;
+    private Timestamp originalCreatedTime;
+    private Timestamp originalUpdatedTime;
     private Long viewCount;
     private Long viewUser;
     private Long createdBy;
