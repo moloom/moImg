85f2379266a538e2b3546d305d27f85987ccd10e
moloom <moloom777@gmail.com>
2024-11-01 00:31:54 +0800

    add some members

--- api/src/main/java/com/moloom/img/api/vo/FileUploadVo.java
+++ api/src/main/java/com/moloom/img/api/vo/FileUploadVo.java
@@ -10,8 +10,11 @@ import org.springframework.web.multipart.MultipartFile;
  */
 @Data
 public class FileUploadVo {
-    private String fileName;    //如果是在页面上传文件，页面会获取文件名，若是api则需自己获取
-    private String fileExtension; //如果是在页面上传文件，页面会获取后缀，若是api则需自己获取
+    private String fileName;    //文件名，如果是在页面上传文件，页面会获取文件名，若是api则需自己获取
+    private String fileExtension; //文件的拓展名，如果是在页面上传文件，页面会获取后缀，若是api则需自己获取
+    private String contentType;     //文件类型；例：application/x-gtar、video/mp4
+    //    private StringBuilder fileStorageName;  //实际存储的文件名
+    private String fileStoragePath;  //存储路径
     private String bucketName;  //存储的bucket名
     private MultipartFile multipartFile;
     private String token;
