b6f50b6f0901d372cb0f41f59ba4ee8f4d199226
moloom <moloom777@gmail.com>
2024-11-04 01:45:27 +0800

    add method download

--- api/src/main/java/com/moloom/img/api/service/ImgHandlerService.java
+++ api/src/main/java/com/moloom/img/api/service/ImgHandlerService.java
@@ -1,8 +1,11 @@
 package com.moloom.img.api.service;
 
 import com.moloom.img.api.entity.ImgInfo;
+import com.moloom.img.api.to.DownloadTO;
 import com.moloom.img.api.to.R;
 import com.moloom.img.api.vo.FileUploadVo;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.ResponseEntity;
 
 import java.io.IOException;
 
@@ -19,7 +22,7 @@ public interface ImgHandlerService {
      * @return
      * @author moloom
      * @date 2024-10-22 20:19:06
-     * @description 处理图片
+     * @description 处理上传图片
      */
     public R imghandler(FileUploadVo fileUploadVo);
 
@@ -31,6 +34,15 @@ public interface ImgHandlerService {
      * @description 保存图片
      */
     public R saveImg(ImgInfo img);
+
+    /**
+     * @author moloom
+     * @date 2024-11-04 00:34:04
+     * @param downloadTO
+     * @return
+     * @description 从服务器返回文件
+     */
+    public ResponseEntity<InputStreamResource> download(DownloadTO downloadTO);
 
 
 }
