33286a6bbefc5b7c535192396a4e3c7109122610
moloom <moloom777@gmail.com>
2024-11-17 22:45:54 +0800

    add comments

--- api/src/main/java/com/moloom/img/api/dao/ImgCameraInfoDao.java
+++ api/src/main/java/com/moloom/img/api/dao/ImgCameraInfoDao.java
@@ -22,12 +22,33 @@ public interface ImgCameraInfoDao {
      */
     public int insert(ImgCameraInfo imgCameraInfo);
 
+    /**
+     * @param imgCameraInfo
+     * @return
+     * @author moloom
+     * @date 2024-11-17 20:05:56
+     * @description update a record of imgCameraInfo,only update not null and not empty fields!
+     */
     public int update(ImgCameraInfo imgCameraInfo);
 
+    /**
+     * @author moloom
+     * @date 2024-11-17 20:23:42
+     * @param id
+     * @return
+     * @description delete a record of imgCameraInfo
+     */
     @Delete("DELETE FROM img_camera_info WHERE img_camera_info_id = #{id}")
     public int deleteById(Long id);
 
 
+    /**
+     * @author moloom
+     * @date 2024-11-17 20:28:00
+     * @param id
+     * @return
+     * @description select a record of imgCameraInfo
+     */
     @Select("SELECT * FROM img_camera_info WHERE img_camera_info_id = #{id}")
     ImgCameraInfo selectOneById(Long id);
 
