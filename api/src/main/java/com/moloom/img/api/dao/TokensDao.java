42fb9e802a98ab0b4e74e1211b09fb5aa63d1888
moloom <moloom777@gmail.com>
2024-11-18 21:44:40 +0800

    add delete method

--- api/src/main/java/com/moloom/img/api/dao/TokensDao.java
+++ api/src/main/java/com/moloom/img/api/dao/TokensDao.java
@@ -1,6 +1,7 @@
 package com.moloom.img.api.dao;
 
 import com.moloom.img.api.entity.Tokens;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 
 /**
@@ -11,11 +12,30 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface TokensDao {
     /**
-     * @author moloom
-     * @date 2024-11-18 20:52:57
      * @param token
      * @return
+     * @author moloom
+     * @date 2024-11-18 20:52:57
      * @description insert a record into tokens table
      */
     public int insert(Tokens token);
+
+    /**
+     * @param token token object
+     * @return
+     * @author moloom
+     * @date 2024-11-18 21:40:20
+     * @description update a record in tokens table
+     */
+    public int update(Tokens token);
+
+    /**
+     * @param token
+     * @return
+     * @author moloom
+     * @date 2024-11-18 21:43:25
+     * @description delete a record in tokens table by token
+     */
+    @Delete("delete from tokens where token = {token}")
+    public int deleteByToken(String token);
 }
