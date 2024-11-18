920ed87288c02ba1e44e904e05dab6753e84b03e
moloom <moloom777@gmail.com>
2024-11-18 21:30:39 +0800

    rename userId to createdBy

--- api/src/main/java/com/moloom/img/api/entity/Tokens.java
+++ api/src/main/java/com/moloom/img/api/entity/Tokens.java
@@ -17,8 +17,8 @@ import java.sql.Timestamp;
 public class Tokens {
     private Long tokenId;
     private String token;
-    private Long userId;
     private Byte status;        //0 无效；1 临时有效，未绑定users,且未存入图片；2 临时有效，未绑定users,但已存入图片；3 已绑定users,user的临时token；4 已绑定users,user的永久token
+    private Long createdBy;
     private Timestamp createdTime;
     private Timestamp updatedTime;
 }
