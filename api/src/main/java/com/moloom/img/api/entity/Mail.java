f34f8a92fb0a8635977df72349b57e32da338b04
moloom <moloom777@gmail.com>
2024-11-18 21:21:27 +0800

    add comments

--- api/src/main/java/com/moloom/img/api/entity/Mail.java
+++ api/src/main/java/com/moloom/img/api/entity/Mail.java
@@ -4,6 +4,11 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
+/**
+ * @author moloom
+ * @date 2024-9-28 21:50:45
+ * @description 邮箱实体类，不入数据库
+ */
 @Data
 @Builder
 @Accessors(chain = true)
