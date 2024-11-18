bebd89993fa5128e1970e3ff615d25ea300c1587
moloom <moloom777@gmail.com>
2024-11-18 20:52:12 +0800

    rename to Tokens and add member primary key tokenId

--- api/src/main/java/com/moloom/img/api/entity/Tokens.java
+++ api/src/main/java/com/moloom/img/api/entity/Tokens.java
@@ -14,9 +14,10 @@ import java.sql.Timestamp;
 @Data
 @Builder
 @Accessors(chain = true)
-public class Token {
+public class Tokens {
+    private Long tokenId;
     private String token;
     private Long userId;
-    private Byte status;
+    private Byte status;        //0 无效；1 临时有效，未绑定users,且未存入图片；2 临时有效，未绑定users,但已存入图片；3 已绑定users,user的临时token；4 已绑定users,user的永久token
     private Timestamp createdTime;
 }
