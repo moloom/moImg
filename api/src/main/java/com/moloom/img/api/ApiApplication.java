52c11effda8fc879a0cfa169f02412ab4398f984
moloom <moloom777@gmail.com>
2024-11-26 19:13:57 +0800

    enable annotation @MapperScan

--- api/src/main/java/com/moloom/img/api/ApiApplication.java
+++ api/src/main/java/com/moloom/img/api/ApiApplication.java
@@ -1,9 +1,11 @@
 package com.moloom.img.api;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.moloom.img.api.dao")
 public class ApiApplication {
 
     public static void main(String[] args) {
