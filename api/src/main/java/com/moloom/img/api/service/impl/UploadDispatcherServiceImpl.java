e503758e341db373781c3c71467b093bfae606fa
moloom <moloom777@gmail.com>
2024-12-02 02:27:09 +0800

    Add: Multiple file upload

--- api/src/main/java/com/moloom/img/api/service/impl/UploadDispatcherServiceImpl.java
+++ api/src/main/java/com/moloom/img/api/service/impl/UploadDispatcherServiceImpl.java
@@ -1,5 +1,6 @@
 package com.moloom.img.api.service.impl;
 
+import com.moloom.img.api.exception.BadRequestException;
 import com.moloom.img.api.exception.ExtensionMismatchException;
 import com.moloom.img.api.service.*;
 import com.moloom.img.api.to.R;
@@ -9,10 +10,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.tika.Tika;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * @author: moloom
@@ -34,7 +39,7 @@ public class UploadDispatcherServiceImpl implements UploadDispatcherService {
 
 
     @Override
-    public R uploadDispatcher(@NotNull UploadVo vo) {
+    public R uploadDispatcher(@NotNull UploadVo vo, MultipartFile[] multipartFiles) {
         /**
          * 查看这请求有没有问题
          * 1.获取文件类型
@@ -44,25 +49,46 @@ public class UploadDispatcherServiceImpl implements UploadDispatcherService {
          */
 
         Tika tika = new Tika();
-        //前端已经实现了获取文件类型；如果是前端页面请求：就不需要再获取一次文件类型；api请求才需要获取类型
-        if (vo.getContentType() == null || vo.getContentType().isEmpty())
-            try (InputStream inputStream = vo.getMultipartFile().getInputStream()) {
-                String contentType = tika.detect(inputStream);
-                log.info("tike获取的类型：{}", contentType);
-                if (contentType != null)
-                    vo.setContentType(contentType);
-            } catch (IOException e) {
+        ArrayList<R> handlerResult = new ArrayList<R>();
+        log.info("number of upload file is {}", multipartFiles.length);
+        //开始处理每个上传的文件
+        Arrays.stream(multipartFiles).forEach(file -> {
+            //把文件信息存入单个vo
+            UploadVo uploadVo;
+            try {
+                uploadVo = (UploadVo) vo.clone();
+            } catch (CloneNotSupportedException e) {
                 throw new RuntimeException(e);
             }
+            uploadVo.setMultipartFile(file);
+            //用 Tika 获取文件类型
+            try (InputStream inputStream = file.getInputStream()) {
+                String contentType = tika.detect(inputStream);
+                log.info("tike获取的类型：{}", contentType);
+                if (contentType == null)
+                    throw new BadRequestException();
+                uploadVo.setContentType(contentType);
+            } catch (Exception e) {
+                throw new BadRequestException("Some errors have been detected in the uploaded files. Please check if the uploaded files are of a valid image, video, or zip type");
+            }
 
-        //匹配文件类型对应的处理方法
-        if (vo.getContentType().startsWith("image/"))
-            return imgHandlerService.imghandler(vo);
-        else if (vo.getContentType().startsWith("video/"))
-            return videoHandlerService.videoHandler(vo);
-        else if (vo.getContentType().startsWith("application/")) {
-            return archiveHandler(vo);
-        } else throw new ExtensionMismatchException();
+            //匹配文件类型对应的处理方法
+            if (uploadVo.getContentType().startsWith("image/"))
+                handlerResult.add(imgHandlerService.imghandler(uploadVo));
+            else if (uploadVo.getContentType().startsWith("video/"))
+                handlerResult.add(videoHandlerService.videoHandler(uploadVo));
+            else if (uploadVo.getContentType().equals("application/octet-stream"))
+                //未上传文件的处理
+                throw new BadRequestException("file not upload");
+            else if (uploadVo.getContentType().startsWith("application/")) {
+                handlerResult.add(archiveHandler(uploadVo));
+            } else throw new ExtensionMismatchException();
+        });
+
+        //检查处理结果，如果全成功则返回成功，否则返回失败。且都返回每个文件的处理结果
+        boolean b = handlerResult.stream().allMatch(r -> r.getStatus() == HttpStatus.OK.value());
+        System.out.println(b);
+        return R.success();
     }
 
     @Override
