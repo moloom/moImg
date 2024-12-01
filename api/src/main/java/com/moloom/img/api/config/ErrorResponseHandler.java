40bb0fe27bcf82a93fd029f7f9831a134dee663b
moloom <moloom777@gmail.com>
2024-12-02 02:19:27 +0800

    add:way to handle my custom exception class

--- api/src/main/java/com/moloom/img/api/config/ErrorResponseHandler.java
+++ api/src/main/java/com/moloom/img/api/config/ErrorResponseHandler.java
@@ -1,5 +1,7 @@
 package com.moloom.img.api.config;
 
+import com.moloom.img.api.exception.BadRequestException;
+import com.moloom.img.api.exception.ExtensionMismatchException;
 import com.moloom.img.api.to.R;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -13,10 +15,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @date: 2024-11-21 00:05
  * @description: error response handler
  */
-//@ControllerAdvice
+@ControllerAdvice
 @Slf4j
 public class ErrorResponseHandler {
-    // 处理所有500错误
+
+
+    @ExceptionHandler({ExtensionMismatchException.class, BadRequestException.class})
+    @ResponseBody
+    public ResponseEntity<R> handleExtensionMismatchException(Exception ex) {
+        // 打印异常栈信息
+        log.error(ex.getMessage(), ex);
+        // 返回自定义的错误信息
+        return new ResponseEntity<>(R.error(HttpStatus.BAD_REQUEST, ex.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    // 对系统内部所有错误统一处理，返回500和固定错误信息
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public ResponseEntity<R> handleInternalServerError(Exception ex) {
