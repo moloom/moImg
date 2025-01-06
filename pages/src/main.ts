6aa1374224cbe779f2b10a26656923491a3d95f9
moloom <moloom777@gmail.com>
2025-01-06 23:13:57 +0800

    add multipart base URL

--- pages/src/main.ts
+++ pages/src/main.ts
@@ -8,6 +8,8 @@ const app = createApp(App)
 const pinia = createPinia();
 // 设置全局的 axios 默认配置
 axios.defaults.baseURL = 'https://img.moloom.cn/api';
+// axios.defaults.baseURL = 'https://moloom.com/api';
+// axios.defaults.baseURL = 'http://localhost:8803/api';
 //axios.defaults.headers.common['Content-Type'] = 'application/json'; // 根据需要设置请求头
 
 // 全局设置 axios
